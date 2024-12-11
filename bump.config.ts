eb3e31533634e72fae3b661478dc8fba066c40ee
TwoKe <twoke945@163.com>
2024-12-11 19:46:55 +0800

    chore: 更新play版本

--- bump.config.ts
+++ bump.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   files: [
     'package.json',
     'packages/core/package.json',
-    'packages/types/package.json',
-    'playground/split-pages-config/package.json',
+    'playgrounds/split-pages-config/package.json',
   ],
 })
